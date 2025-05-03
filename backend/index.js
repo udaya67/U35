d1c932ef04ed42c0ccc510f99a7a8df1c852976b
udaya <udayach67@gmail.com>
2025-05-03 09:54:58 -0400

    secured credentials

--- backend/index.js
+++ backend/index.js
@@ -14,10 +14,10 @@ app.use(express.json());
 
 // Database connection
 const pool = mysql.createPool({
-        host: 'sql5.freesqldatabase.com',
-        user: 'sql5775566',
-        password: 'RDxanvSZPt',
-        database: 'sql5775566',
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
         waitForConnections: true,
         connectionLimit: 10,
         queueLimit: 0
